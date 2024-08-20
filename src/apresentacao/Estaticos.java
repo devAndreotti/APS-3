ad5c2eb96342d6a66d461bb03f2fb690a63ef59b
monique mendes <111323380+moniquedmendes@users.noreply.github.com>
2024-08-20 13:32:03 -0300

    Update Estaticos.java

--- src/apresentacao/Estaticos.java
+++ src/apresentacao/Estaticos.java
@@ -1,14 +1,1 @@
 
-package apresentacao;
-
-
-public class Estaticos
-{
-    public static int SOMA;
-    public static String TEXTO;
-    public static String MENSAGEM;
-    public static String NOME;
-    public static String RESP1;
-    public static int SOMAACERTOS;
-    public static String ACERTOS;
-}
